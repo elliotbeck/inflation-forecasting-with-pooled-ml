b2fb23ec892ca312af364e0a2bf6f7eeb9a10c0a
Elliot Beck <elliot.beck@hotmail.com>
2025-04-02 15:49:34 +0000

    Update main file to include regional dummies

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ from src.data.global_inflation_loader import (
     load_global_inflation_data,
     get_lagged_features_and_target,
 )
+from src.features.create_regional_dummies import create_regional_dummies
 from src.config import NUM_LAGS
 
+# Load and prepare data
 data = load_global_inflation_data("data/Inflation-data.xlsx")
 data_tabular = get_lagged_features_and_target(df=data, n_lags=NUM_LAGS)
+data_tabular = create_regional_dummies(data_tabular, "data/country_continent_map.csv")
 
+# Run comparison
 results = compare_models(data, data_tabular)
 
+# Save the results to csv
 results.to_csv("results/results.csv")
