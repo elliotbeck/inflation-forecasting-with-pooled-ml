f5c0bf441792618883bd7626efc2bbd15f576d5f
Elliot Beck <elliot.beck@htomail.com>
2025-04-02 02:27:59 +0000

    Use global inflation data

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from src.benchmark.compare_models import compare_models  # noqa
-from src.data.fred_loader import load_fred_data  # noqa
-import yaml
+from src.benchmark.compare_models import compare_models
+from src.data.global_inflation_loader import (
+    load_global_inflation_data,
+    get_lagged_features_and_target,
+)
+from src.config import NUM_LAGS
 
-with open("src/config.yaml", "r") as f:
-    config = yaml.safe_load(f)
+data = load_global_inflation_data("data/Inflation-data.xlsx")
+data_tabular = get_lagged_features_and_target(df=data, n_lags=NUM_LAGS)
 
-data = load_fred_data(config["countries"])
-results = compare_models(data)
+results = compare_models(data, data_tabular)
 
 results.to_csv("results/results.csv")
