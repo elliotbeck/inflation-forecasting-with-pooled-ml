7dc81611f83e71d2fa572ab0d1eaca8cebf1a55c
Elliot Beck <elliot.beck@hotmail.com>
2025-04-02 19:27:05 +0000

    Fix import and sampling

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from benchmark.compare_models import compare_models
-from data.global_inflation_loader import (
+from data_loaders.global_inflation_loader import (
     load_global_inflation_data,
     get_lagged_features_and_target,
 )
-from features.create_dummies import create_regional_dummies
 from config import NUM_LAGS
 
 # Load and prepare data
 data = load_global_inflation_data("data/Inflation-data.xlsx")
 data_tabular = get_lagged_features_and_target(df=data, n_lags=NUM_LAGS)
-data_tabular = create_regional_dummies(data_tabular, "data/country_continent_map.csv")
+
+# Sample 30 countries for testing TODO: remove this
+sampled_countries = data.sample(n=30, random_state=42).columns
+data = data[sampled_countries]
+data_tabular = data_tabular[data_tabular["Country"].isin(sampled_countries)]
+
 
 # Run comparison
 results = compare_models(data, data_tabular)
